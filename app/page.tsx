4d6d26640065857f327bb496dac7b5845d94c62f
Francisco Aceves <frenagon@gmail.com>
2025-04-26 16:23:22 -0600

    feat: expand color palette and add section dividers

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
 export default function Home() {
   return (
     <div className="flex h-screen w-screen items-center justify-center">
-      <div className="h-screen w-screen bg-primary-500 md:aspect-9/16 md:h-5/6 md:w-auto md:rounded-2xl md:shadow-[0_35px_50px_rgba(0,0,0,0.25)]"></div>
+      <div className="flex h-screen w-screen flex-col overflow-hidden bg-primary-400 md:aspect-9/16 md:h-5/6 md:w-auto md:rounded-2xl md:shadow-[0_35px_50px_rgba(0,0,0,0.25)]">
+        <div className="h-2/6 w-full bg-secondary-400" />
+        <div className="h-1/6 w-full bg-primary-400" />
+        <div className="h-2/6 w-full bg-secondary-400" />
+        <div className="h-1/6 w-full bg-primary-400" />
+      </div>
     </div>
   );
 }
