d4048b6ac6d65da978e4d5f79e3bada59b92aced
Francisco Aceves <frenagon@gmail.com>
2025-04-26 14:32:56 -0600

    feat: create responsive card

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <></>;
+  return (
+    <div className="flex h-screen w-screen items-center justify-center">
+      <div className="h-screen w-screen bg-primary-500 md:aspect-9/16 md:h-5/6 md:w-auto md:rounded-2xl md:shadow-[0_35px_50px_rgba(0,0,0,0.25)]"></div>
+    </div>
+  );
 }
