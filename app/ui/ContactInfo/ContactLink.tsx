0d002d1cee99f9334120576392aaaef1457e7704
Francisco Aceves <frenagon@gmail.com>
2025-04-27 17:11:11 -0600

    feat: make link hover background lighter

--- app/ui/ContactInfo/ContactLink.tsx
+++ app/ui/ContactInfo/ContactLink.tsx
@@ -13,7 +13,7 @@ export default function ContactLink({
     <a
       href={href}
       target="_blank"
-      className="flex items-center py-2 hover:bg-primary-300"
+      className="flex items-center py-2 hover:bg-primary-200"
     >
       <Image className="ml-6 w-10" {...imageData} />
       <span className="ml-4">{text}</span>
