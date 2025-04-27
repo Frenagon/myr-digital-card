8a1e5ef3019d5dbbeeb9e76c8dcf355f928fb51a
Francisco Aceves <frenagon@gmail.com>
2025-04-27 16:29:19 -0600

    feat: change title

--- app/ui/title.tsx
+++ app/ui/title.tsx
@@ -1,9 +1,8 @@
 export default function Title({ className }: { className: string }) {
   return (
-    <div className={`m-6 ${className}`}>
-      <h1>Myriam Cárdenas</h1>
-      <h2>Medicina y Nutrición</h2>
-      <h2>Medimyr</h2>
+    <div className={`m-6 flex flex-col justify-center ${className}`}>
+      <h1>Dra. Myriam Cárdenas</h1>
+      <h2>Nutrición Clínica</h2>
     </div>
   );
 }
