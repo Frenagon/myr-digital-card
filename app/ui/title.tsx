a4bdd863fd34f5c09cc9ae37bbaf99780eaa1a25
Francisco Aceves <frenagon@gmail.com>
2025-04-26 22:44:37 -0600

    feat: add title

---
+++ app/ui/title.tsx
@@ -0,0 +1,9 @@
+export default function Title({ className }: { className: string }) {
+  return (
+    <div className={`m-6 ${className}`}>
+      <h1>Myriam Cárdemas</h1>
+      <h2>Medicina y Nutricion</h2>
+      <h2>Medimyr</h2>
+    </div>
+  );
+}
