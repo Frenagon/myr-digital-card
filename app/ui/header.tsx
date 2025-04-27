aa1bd42803e1131dd5fb4d9ac6e66b547b469498
Francisco Aceves <frenagon@gmail.com>
2025-04-27 17:09:47 -0600

    feat: remove logo

--- app/ui/header.tsx
+++ app/ui/header.tsx
@@ -2,24 +2,16 @@ import Image from "next/image";
 
 export default function Header({ className }: { className: string }) {
   return (
-    <div className={`flex flex-col ${className}`}>
-      <div className="flex flex-1 items-center justify-center bg-gray-200">
-        <Image
-          src="/logo.png"
-          alt="Un estetoscopio con una cara feliz"
-          height={548}
-          width={549}
-          sizes="100vw"
-          className="w-2/7"
-        />
-      </div>
+    <div
+      className={`flex flex-col items-center justify-center bg-gradient-to-b from-neutral-100 to-neutral-200 ${className}`}
+    >
       <Image
         src="/profile-picture.png"
         alt="La doctora Myriam posando en su consultorio"
         height={1542}
         width={2048}
         sizes="100vw"
-        className="-mt-[calc(25%/2)] ml-6 aspect-square w-1/4 self-start rounded-full object-cover"
+        className="aspect-square w-1/3 rounded-full object-cover"
       />
     </div>
   );
--- app/ui/title.tsx
+++ app/ui/title.tsx
@@ -1,6 +1,6 @@
 export default function Title({ className }: { className: string }) {
   return (
-    <div className={`m-6 flex flex-col justify-center ${className}`}>
+    <div className={`mx-6 flex flex-col justify-center ${className}`}>
       <h1>Dra. Myriam Cárdenas</h1>
       <h2>Nutrición Clínica</h2>
     </div>
