c9fadf2486c107468783b0ee0bf47e1d2184e02c
Francisco Aceves <frenagon@gmail.com>
2025-04-27 14:48:28 -0600

    fix: fix typo on title

--- app/ui/title.tsx
+++ app/ui/title.tsx
@@ -2,7 +2,7 @@ export default function Title({ className }: { className: string }) {
   return (
     <div className={`m-6 ${className}`}>
       <h1>Myriam Cárdenas</h1>
-      <h2>Medicina y Nutricion</h2>
+      <h2>Medicina y Nutrición</h2>
       <h2>Medimyr</h2>
     </div>
   );
