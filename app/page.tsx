9f2fef9b1599900b1416289a82f8a136b2d4bc5c
Francisco Aceves <frenagon@gmail.com>
2025-04-27 20:07:06 -0600

    feat: add minimum width

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import ContactInfo from "@/app/ui/ContactInfo/ContactInfo";
 export default function Home() {
   return (
     <div className="flex h-screen min-h-[800px] w-screen items-center justify-center">
-      <div className="flex h-full w-full flex-col overflow-hidden bg-neutral-100 md:aspect-9/16 md:h-7/8 md:w-auto md:rounded-2xl md:shadow-[0_35px_50px_rgba(0,0,0,0.3)]">
+      <div className="flex h-full w-full min-w-[380px] flex-col overflow-hidden bg-neutral-100 md:aspect-9/16 md:h-7/8 md:w-auto md:rounded-2xl md:shadow-[0_35px_50px_rgba(0,0,0,0.3)]">
         <Header className="h-2/8" />
         <Title className="h-2/8" />
         <ContactInfo className="h-4/8" />
