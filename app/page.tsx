171ac6c400f0f453ce8f4248a9eb983a5a5f91f9
Francisco Aceves <frenagon@gmail.com>
2025-04-27 19:43:36 -0600

    feat: add logo

--- app/page.tsx
+++ app/page.tsx
@@ -6,9 +6,9 @@ export default function Home() {
   return (
     <div className="flex h-screen min-h-[800px] w-screen items-center justify-center">
       <div className="flex h-full w-full flex-col overflow-hidden bg-neutral-100 md:aspect-9/16 md:h-7/8 md:w-auto md:rounded-2xl md:shadow-[0_35px_50px_rgba(0,0,0,0.3)]">
-        <Header className="h-2/7" />
-        <Title className="h-1/7" />
-        <ContactInfo className="h-4/7" />
+        <Header className="h-2/8" />
+        <Title className="h-2/8" />
+        <ContactInfo className="h-4/8" />
       </div>
     </div>
   );
--- app/ui/header.tsx
+++ app/ui/header.tsx
@@ -3,15 +3,23 @@ import Image from "next/image";
 export default function Header({ className }: { className: string }) {
   return (
     <div
-      className={`flex flex-col items-center justify-center bg-gradient-to-b from-primary-100 to-primary-200 ${className}`}
+      className={`relative mb-4 flex flex-col items-center justify-center bg-gradient-to-b from-primary-100 to-primary-200 ${className}`}
     >
+      <Image
+        src="/logo.svg"
+        alt="La doctora Myriam posando en su consultorio"
+        height={1542}
+        width={2048}
+        sizes="100vw"
+        className="aspect-square h-2/3"
+      />
       <Image
         src="/profile-picture.png"
         alt="La doctora Myriam posando en su consultorio"
         height={1542}
         width={2048}
         sizes="100vw"
-        className="aspect-square w-1/3 rounded-full object-cover"
+        className="absolute -bottom-1/4 left-0 ml-4 aspect-square h-1/2 w-auto rounded-full object-cover"
       />
     </div>
   );
